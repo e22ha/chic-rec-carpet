26cf9d578f9b7ddc5c5ce02a1e80ca3fd167f2bd
Sergey Manzhos <sergey.manzhos@outlook.com>
2024-03-26 11:35:22 +0700

    add image, rewrite index.ts: load file func

--- index.ts
+++ index.ts
@@ -10,32 +10,17 @@ async function Handler(request: Request): Promise<Response> {
   const url = new URL(request.url);
   if (url.pathname == "" || url.pathname == "/")
     return new Response(Bun.file("index.html"));
-  else if (url.pathname == "/main.js") return new Response(Bun.file("main.js"));
-  else if (url.pathname == "/index.css")
-    return new Response(Bun.file("index.css"));
-  else if (url.pathname == "/time.js") return new Response(Bun.file("time.js"));
+  else if (url.pathname == "/main.js") return serveFile("main.js");
+  else if (url.pathname == "/index.css") return serveFile("index.css");
+  else if (url.pathname == "/time.js") return serveFile("time.js");
   else if (url.pathname == "/three.module.min.js")
-    return new Response(
-      Bun.file("node_modules/three/build/three.module.min.js"),
-    );
+    return serveFile("node_modules/three/build/three.module.min.js");
   else if (url.pathname == "/tween.esm.js")
-    return new Response(
-      Bun.file("node_modules/@tweenjs/tween.js/dist/tween.esm.js"),
-    );
-  else if (url.pathname == "/img/1section_img.png")
-    return new Response(
-      Bun.file("img/1section_img.png", { type: "image/png" }),
-    );
-  else if (url.pathname == "/img/btn/order_btn.png")
-    return new Response(
-      Bun.file("img/btn/order_btn.png", { type: "image/png" }),
-    );
-  else if (url.pathname == "/animation.js")
-    return new Response(Bun.file("animation.js"));
-  else if (url.pathname == "/img/placeholder_1.png")
-    return new Response(
-      Bun.file("img/placeholder_1.png", { type: "image/png" }),
-    );
+    return serveFile("node_modules/@tweenjs/tween.js/dist/tween.esm.js");
+  else if (url.pathname.startsWith("/img/")){
+    console.log(url.pathname.substring(1))
+    return serveFile(url.pathname.substring(1));
+  }
   else if (request.method == "POST" && url.pathname == "/log-scroll") {
     const formData = await request.formData();
     const message = formData.get("message");
@@ -52,4 +37,30 @@ async function Handler(request: Request): Promise<Response> {
     });
   } else console.log(url.pathname);
   return new Response("Not found", { status: 404 });
+}
+
+function serveFile(filePath: string): Response {
+  const file = Bun.file(filePath);
+  const contentType = getContentType(filePath);
+  return new Response(file, { headers: { "Content-Type": contentType } });
+}
+
+function getContentType(filePath: string): string {
+  const extension = filePath.split(".").pop();
+  switch (extension) {
+    case "css":
+      return "text/css";
+    case "js":
+      return "application/javascript";
+    case "png":
+      return "image/png";
+    case "jpg":
+      return "image/jpeg";
+    case "jpeg":
+      return "image/jpeg";
+    case "gif":
+      return "image/gif";
+    default:
+      return "application/octet-stream";
+  }
 }
